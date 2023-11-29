3681f52f467f811ca19153341e59c35ea2a9a9ef
Ricardo <ricardo.spinola9@gmail.com>
2023-11-28 22:34:22 -0300

    perfil component

--- front/games_search_app/src/app/components/molecues/Perfil/index.tsx
+++ front/games_search_app/src/app/components/molecues/Perfil/index.tsx
@@ -1,5 +1,15 @@
+import style from './style.module.css'
+
 export function Perfil() {
     return (
-        <div>PERFIL</div>
+        <div class={style.root}>
+            <span class='mr-3 text-right'>
+                <h4 class='text-sm font-medium text-slate-300'>Invoker</h4>
+                <h5 class='text-xs font-medium text-slate-400'>Admin</h5>
+            </span>
+            <div class="w-10 rounded-full overflow-auto">
+                <img src="/invo.jfif" alt="" />
+            </div>
+        </div>
     )
 }
